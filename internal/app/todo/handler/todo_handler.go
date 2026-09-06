4ce8a1a282f9af058c5206ea14b6f3477de843b8
jevvonn <darmawanb985@gmail.com>
2026-09-06 19:50:07 +0700

    fix route group name for todos and update welcome message in API response

--- internal/app/todo/handler/todo_handler.go
+++ internal/app/todo/handler/todo_handler.go
@@ -17,7 +17,7 @@ type TodoHandler struct {
 func NewTodoHandler(router fiber.Router, todoUsecase usecase.TodoUsecase) {
 	h := &TodoHandler{usecase: todoUsecase}
 
-	todos := router.Group("/todo")
+	todos := router.Group("/todos")
 	todos.Post("/", h.Create)
 	todos.Get("/", h.GetAll)
 	todos.Get("/:id", h.GetByID)
--- internal/bootstrap/route.go
+++ internal/bootstrap/route.go
@@ -11,7 +11,7 @@ import (
 
 func registerRoutes(app *fiber.App, db *gorm.DB) {
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "Hello, welcome to the API"})
+		return c.JSON(fiber.Map{"message": "Hello, welcome to the API of ws-cicd-bcc2026!"})
 	})
 
 	app.Get("/hello", func(c *fiber.Ctx) error {
